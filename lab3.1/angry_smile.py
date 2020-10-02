27cdbb5e245ba95416a30a7f608f6d56ef8cc1cc
MaximPovolotskii <povolotskii.mr@phystech.edu>
2020-10-02 11:00:02 +0300

    Made animation of getting angry

--- lab3.1/angry_smile.py
+++ lab3.1/angry_smile.py
@@ -3,27 +3,40 @@ from pygame.draw import *
 
 pygame.init()
 
-FPS = 30
+FPS = 10
 screen = pygame.display.set_mode((400, 400))
 
-rect(screen, (169, 176, 184), (0, 0, 400, 400), 0)
-circle(screen, (255, 255, 51), (200, 200), 150)
-line(screen, (0, 0, 0), (70, 60), (180, 150), 20)
-circle (screen, (255, 0, 0), (150, 165), 25)
-circle (screen, (0, 0, 0), (150, 165), 10)
-line(screen, (0, 0, 0), (220, 150), (330, 90), 20)
-circle (screen, (255, 0, 0), (250, 165), 20)
-circle (screen, (0, 0, 0), (250, 165), 10)
-rect(screen, (0, 0, 0), (120, 260, 160, 30), 0)
+def face(x):
+    """
+    draws a face
+    x adjusts red component of the face colour
+    also x adjusts the eyebrows and the mouth
+    """
+    rect(screen, (169, 176, 184), (0, 0, 400, 400), 0)
+    circle(screen, (255, 255 - 5 * x, 51 - x), (200, 200), 150)
+    line(screen, (0, 0, 0), (70, 60), (180, 150 - 51 + x), 20)
+    circle (screen, (255, 0, 0), (150, 165), 25)
+    circle (screen, (0, 0, 0), (150, 165), 10)
+    line(screen, (0, 0, 0), (220, 150 - 51 + x), (330, 90), 20)
+    circle (screen, (255, 0, 0), (250, 165), 20)
+    circle (screen, (0, 0, 0), (250, 165), 10)
+    rect(screen, (0, 0, 0), (120, 260, 160, 30 - int(x / 2)), 0)
 
 pygame.display.update()
 clock = pygame.time.Clock()
 finished = False
 
+k = 0
 while not finished:
     clock.tick(FPS)
+    face(k)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             finished = True
+    if k < 51:
+        k += 1
+    else:
+        k = 0
+    pygame.display.flip()
 
 pygame.quit()
